7d9fa95bcf6ba438542dfff26ef6b8d93b99385b
RF3\Ijat <me@ijat.my>
2015-02-13 16:19:09 +0800

    Update license

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -11,12 +11,23 @@
 	* May contains examples from PDCurses source file with some modifications.
 	* To compile this program, it require pdcurses.dll (in same folder with exe files) and pdcurses.lib as a linker library.
 	*
-	* LICENSE
-	* This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-	* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+    Copyright (C) 2014-2015 Ijat
 
-	* LAST MODIFIED	: 19/3/14 16:38
-	* LATEST BUILD	: 19/3/14 16:38
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	* LAST MODIFIED	: 13/2/15
+	* LATEST BUILD	: 13/2/15
 	*/
 
 #include <curses.h>								// External library used to increase user experience console interface
